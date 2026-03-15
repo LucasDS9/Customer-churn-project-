9da9d7202d5f2d612b6f53874f0256fb00c34740
LucasDS9 <lucasvinids@gmail.com>
2026-03-15 13:53:04 -0300

    config

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,4 @@
 // API base URL: use when frontend is opened from file or another port
-window.API_BASE = window.API_BASE || "http://127.0.0.1:8000";
+window.API_BASE = window.location.hostname === "localhost"
+  ? "http://127.0.0.1:8000"
+  : "https://customer-churn-project-d7xs.onrender.com";
