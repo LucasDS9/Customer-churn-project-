228d795f09d022567b7e316091c41c6ea4914cdf
LucasDS9 <lucasvinids@gmail.com>
2026-03-22 17:23:21 -0300

    config

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,1 +1,3 @@
-window.API_BASE = "http://127.0.0.1:8000";
+window.API_BASE = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
+  ? "http://127.0.0.1:8000"
+  : "https://customer-churn-project-d7xs.onrender.com";
